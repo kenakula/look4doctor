bbb7651c2b7c217d156fc1a5ae7e7a23602f4a2d
AKenan <kenkosh@yandex.ru>
2022-12-08 01:29:01 +0400

    fixes configs

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,19 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 import { App } from './app/app';
+import { Provider } from 'react-redux';
+import { store } from 'app/store';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
-root.render(<App />);
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
