41a85deaa7c2de6adce8b2eb9515eae5e4fd59f4
AKenan <kenkosh@yandex.ru>
2022-12-09 00:22:23 +0400

    deletes payload

--- backend/src/access/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './check-role';
-export * from './is-admin';
-export * from './is-admin-or-current-user';
--- backend/src/globals/global-settings.ts
+++
@@ -1,9 +0,0 @@
-import { GlobalConfig } from 'payload/types';
-
-const GlobalSettings: GlobalConfig = {
-  slug: 'globalSettings',
-  label: 'Глобальные настройки',
-  fields: [],
-};
-
-export default GlobalSettings;
