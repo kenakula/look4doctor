66b0fa6adacd21eb17b7fca0109fedd214d659f7
AKenan <kenkosh@yandex.ru>
2022-12-08 22:42:07 +0400

    gitignore update

--- backend/dist/globals/global-settings.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var GlobalSettings = {
-    slug: 'globalSettings',
-    label: 'Глобальные настройки',
-    fields: [],
-};
-exports.default = GlobalSettings;
